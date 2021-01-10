9eeecd3b66c4fcd6c25e05dbc4456c7a7b676956
Artur Pata <artur.pata@gmail.com>
2021-01-10 15:15:56 +0200

    [Feat] Remove boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>Sensor Panel</h1>
+    <p>View data from building sensors</p>
+  </div>
+);
 
 export default App;
